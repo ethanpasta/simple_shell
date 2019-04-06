8431aceb2a93f9ec86874aabef6d4c4f0ead8e2b
ethanpasta <ethan.mayer9@gmail.com>
2019-04-06 21:07:41 +0000

    Function prototypes

--- shell_head.h
+++ shell_head.h
@@ -4,5 +4,10 @@
 int _strlen(char *);
 int _strcmp(char *, char *);
 int _puts(char *);
+char **strtow(char *, char);
+int word_count(char *, char);
+char *find_path(char **);
+char *check_file_withP(char **, char *);
+char *str_concat(char *, char *);
 
 #endif
