7b4e04405489c91314b474c4b865eff6027c041c
ethanpasta <ethan.mayer9@gmail.com>
2019-04-06 21:08:35 +0000

    str_concat function

--- string_f.c
+++ string_f.c
@@ -11,6 +11,8 @@
  */
 int _strlen(char *str)
 {
+	if (!str)
+		return (-1);
 	if (*str == '\0')
 		return (0);
 	return (_strlen(str + 1) + 1);
@@ -26,6 +28,8 @@ int _puts(char *buff)
 {
 	size_t len;
 
+	if (!buff)
+		return (-1);
 	len = _strlen(buff);
 	if (write(1, buff, len) == -1)
 		return (-1);
@@ -41,9 +45,39 @@ int _puts(char *buff)
  */
 int _strcmp(char *str1, char *str2)
 {
+	if (!str1 || !str2)
+		return (0);
 	if (!*str1 && !*str2)
 		return (1);
 	if (*str1 != *str2)
 		return (0);
 	return (_strcmp(str1 + 1, str2 + 1));
+}
+
+/**
+ * str_concat - function concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to a newly allocated space in memory which
+ * contains the contents of s1, followed by s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, size1 = 0, size2 = 0, pos = 0;
+	char *s;
+
+	if (!s1 || !s2)
+		return (NULL);
+	size1 =  _strlen(s1);
+	size2 = _strlen(s2);
+	s = malloc(sizeof(char) * (size1 + size2 + 1));
+	if (!s)
+		return (NULL);
+	while (i < size1)
+		s[pos++] = s1[i++];
+	while (j < size2)
+		s[pos++] = s2[j++];
+	s[pos] = '\0';
+	return (s);
 }
