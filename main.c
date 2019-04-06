042a64127edb3f897253b9b816b039924d387e4c
ethanpasta <ethan.mayer9@gmail.com>
2019-04-06 21:07:29 +0000

    Add path option and command line arguments

--- main.c
+++ main.c
@@ -7,13 +7,14 @@
 #include <stdlib.h>
 #include "shell_head.h"
 
-int main(int __attribute__((unused)) ac, char **av)
+int main(int __attribute__((unused)) ac, char **av, char **env)
 {
 	char *buffer;
+	char **args;
+	char *command;
 	size_t buffer_size;
 	size_t real_size;
 	pid_t child_p;
-	int check;
 
 	signal(SIGINT, SIG_IGN);
 	buffer_size = 32;
@@ -30,14 +31,16 @@ int main(int __attribute__((unused)) ac, char **av)
 			break;
 		real_size = _strlen(buffer);
 		buffer[real_size - 1] = '\0';
+		args = strtow(buffer, ' ');
+		command = check_file_withP(env, args[0]);
 		child_p = fork();
 		if (child_p == 0)
 		{
-			if (execve(buffer, av, NULL) == -1)
+			if (execve(command, args, NULL) == -1)
 			{
 				perror(av[0]);
 				free(buffer);
-				return 0;
+				exit(99);
 			}
 		}
 		else
