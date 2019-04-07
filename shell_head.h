49aa9c8ffd400a5285004a120f03e90f2cd4d0c2
VieetBubbles <537@holbertonschool.com>
2019-04-07 01:30:26 +0000

    Added function prototypes for built-ins

--- shell_head.h
+++ shell_head.h
@@ -1,6 +1,12 @@
 #ifndef SHELL_HEAD
 #define SHELL_HEAD
 
+typedef struct built
+{
+	char *built_in;
+	void (*f)(char**);
+} built_t;
+
 int _strlen(char *);
 int _strcmp(char *, char *);
 int _puts(char *);
@@ -9,5 +15,10 @@ int word_count(char *, char);
 char *find_path(char **);
 char *check_file_withP(char **, char *);
 char *str_concat(char *, char *);
+int do_built_in(char *arg, char** env, built_t a[]);
+void print_env(char** env);
+void set_env(char** env);
+void unset_env(char** env);
+void change_dir(char** env);
 
 #endif
