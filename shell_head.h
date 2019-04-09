8fb8d4763004cd583c2a40064a8a0e8e1044066a
VieetBubbles <537@holbertonschool.com>
2019-04-09 21:43:10 +0000

    Place all header files in main header file

--- shell_head.h
+++ shell_head.h
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
 
 /* Struct definition for built in arguments */
 typedef struct built_info
