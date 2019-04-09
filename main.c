f3907a097afdb3767d8423197250a268b51c1f99
VieetBubbles <537@holbertonschool.com>
2019-04-09 21:42:44 +0000

    Remove redundant header files

--- main.c
+++ main.c
@@ -1,10 +1,3 @@
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include "shell_head.h"
 
 /**
