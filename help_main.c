2612b1e20a7f3a619095f4e3b1277fa9f97fb171
ethanpasta <ethan.mayer9@gmail.com>
2019-04-14 15:34:53 +0000

    Change function prototypes

--- help_main.c
+++ help_main.c
@@ -1,6 +1,23 @@
 #include "shell_head.h"
 
 /**
+ * free_array - function frees an array of strings
+ * @arr: array to free
+ *
+ * Return: none
+ */
+void free_array(char **arr)
+{
+	int i;
+
+	if (!arr)
+		return;
+	for (i = 0; arr[i]; i++)
+		free(arr[i]);
+	free(arr);
+}
+
+/**
  * error_msg - function writes message to standard error
  * @line_num: number of line on exit
  * @args: array of arguments
@@ -11,6 +28,7 @@
 void error_msg(size_t line_num, char **args, char **av)
 {
 	char *buff = "";
+	char *line;
 
 	/* check if one of two special error values, if it is create manual
 	   error message */
@@ -18,7 +36,9 @@ void error_msg(size_t line_num, char **args, char **av)
 	{
 		buff = av[0];
 		buff = str_concat(buff, ": ");
-		buff = str_concat(buff, _itoa(line_num));
+		line = _itoa(line_num);
+		buff = str_concat(buff, line);
+		free(line);
 		buff = str_concat(buff, ": ");
 		buff = str_concat(buff, args[0]);
 		buff = str_concat(buff, ": ");
@@ -26,6 +46,7 @@ void error_msg(size_t line_num, char **args, char **av)
 		buff = str_concat(buff, "\n");
 		/* print error message to standard error */
 		_puts(buff, 2);
+		free(buff);
 	}
 	else
 		perror(args[0]);
@@ -61,42 +82,47 @@ char **check_create_args(char **buffer, size_t *buffer_size)
 	(*buffer)[real_size - 1] = '\0';
 	/* split buffer by spaces, into an array of strings */
 	args = strtow(*buffer, ' ');
+	free(*buffer);
+	*buffer = NULL;
 	return (args);
 }
 
 /**
  * child_proc - function takes care of child process. Tries to
  * execute commands from the user
- * @args: array of arguments from user
  * @built_ins: array of built-in structure
  * @av: arguments to file
  * @line_num: current line number
  * @child_p: current process
+ * @in: built in info structure
  *
  * Return: none
  */
-void child_proc(char **args, built_t built_ins[], char **av, size_t line_num, pid_t *child_p)
+void child_proc(built_t built_ins[], char **av, size_t line_num, pid_t *child_p, built_info_t *in)
 {
 	char *command;
-	char **env = built_ins[0].info->env;
 
 	/* check if command is a built in, if it is, go back to main while loop */
-	if (do_built_in(args, env, built_ins) == 1)
+	if (do_built_in(built_ins, in))
+	{
 		return;
+	}
 	*child_p = fork();
 	if (*child_p != 0)
-		return;
-	/* check (and execute) if command is in PATH variable */
-	command = check_file_withP(env, args[0]);
-	if (command && execve(command, args, env) == -1)
 	{
-		error_msg(line_num, args, av);
-		exit(100);
+		return;
+	}
+	/* check (and execute) if command is in PATH variable */
+	command = check_file_withP(in->env_s, in->args[0]);
+	if (command && execve(command, in->args, in->env_s) == -1)
+	{
+		error_msg(line_num, in->args, av);
+		exit(0);
 	}
 	/* check (and execute) if command on its own is a full command path */
-	if (execve(args[0], args, env) == -1)
+	if (execve(in->args[0], in->args, in->env_s) == -1)
 	{
-		error_msg(line_num, args, av);
-		exit(101);
+		error_msg(line_num, in->args, av);
+		exit(0);
 	}
 }
